3b2928d5a74442122133995791c1c52f97b8df7d
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-22 18:11:59 -0500

    Update 2_dataTypes.py

--- 2_dataTypes.py
+++ 2_dataTypes.py
@@ -6,5 +6,28 @@ print('Hello World')
 print("""Hello World""")
 print('''Hello World''')
 
-print(type("Hello World"));
-### 
+print(type("Hello World"))
+
+### NUMBERS
+#Integer
+print(10)
+#Float
+print(13.2)
+
+#BOOLEAN
+print(type(True));
+print(False);
+
+#LISTS / ARRAY
+array = [1,2,3,4,5];
+arrayString = ["Uno","Dos","Tres","Cuatro"];
+arrayMix = [1,True,"Hello",10.2];
+print(arrayString[1]);
+
+# TUPLES
+tuple = (1,2,3,4,5,6);
+print(type(tuple));
+
+#
+
+
