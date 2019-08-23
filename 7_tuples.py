48b6ae53f9810b44570b5b4d4d90013126d7d606
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-23 17:04:03 -0500

    Update Tuples + 8 Sets

---
+++ 7_tuples.py
@@ -0,0 +1,14 @@
+## TUPLAS
+x = (1,2,3);
+months = ('Berlin','Moscú','Nairobi','Tokyo','Rio','Denver','Oslo');
+print(months[1]);
+y = tuple((1,2,3,4,5))
+## del y;
+print(y[0]);
+
+locations = {
+    (29.500,34.900) : "Tokyo",
+    (23.600,30.000) : "New York"
+}
+
+print(locations);
