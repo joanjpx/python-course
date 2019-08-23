b1145b8536f57b0a7f7abdad75918f50998b511d
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-23 17:27:02 -0500

    + 9 Dictionaries

--- 8_sets.py
+++ 8_sets.py
@@ -1,1 +1,10 @@
-## SETS
+## SETS VARIABLES
+cities = {"New York", "London", "Dublin"}
+
+print(cities);
+cities.add('Munich');
+print(cities);
+
+# cities.clear();
+# del cities;
+print('New York' in cities);
---
+++ 9_dictionaries.py
@@ -0,0 +1,24 @@
+### DICTIONARIES
+product = {
+    "name" : "Book",
+    "quantity" : 1,
+    "price" : 4.99
+}
+
+person = {
+    "firstName" : "Joan",
+    "lastName" : "Perez Alvarado"
+}
+## accesing keys
+#print(person['firstName'])
+
+
+# delete object
+## del person;
+
+## Working with keys
+print(person.keys());
+print(person.items());
+
+## Clearing Object
+person.clear();
