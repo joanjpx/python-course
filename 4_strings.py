35dd0c61b31d7aba283f4dd4a82b822b7cc7f9ae
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-23 10:50:15 -0500

    Update 4_strings.py

--- 4_strings.py
+++ 4_strings.py
@@ -4,18 +4,26 @@ myString = "hello world";
 ## Dir para saber lo que se puede hacer con un string // Get Strings Functions
 
 #print(dir(myString));
-print(myString.upper());
-print(myString.lower());
-print(myString.swapcase());
-print(myString.capitalize());
+# print(myString.upper());
+# print(myString.lower());
+# print(myString.swapcase());
+# print(myString.capitalize());
 
-## CHAINED METHODS
-print(myString.replace('hello','bye').upper());
+# ## CHAINED METHODS
+# print(myString.replace('hello','bye').upper());
 
-print(myString.count('o'));
+# print(myString.count('o'));
 
-#Determinar palabras al principio o fin de una cadena.
-print(myString.startswith('hello'));
-print(myString.endswith('world'));
+# #Determinar palabras al principio o fin de una cadena.
+# print(myString.startswith('hello'));
+# print(myString.endswith('world'));
 
-print(myString.split());
+# print(myString.split());
+# print(myString.find('h'));
+# print(myString.index('e'));
+
+# print(myString.isalpha());
+# print(myString.isnumeric());
+# print(len(myString));
+
+print(myString[-1]);
