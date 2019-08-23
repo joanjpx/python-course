f2e5108520a11439a55de9ccb73781c654ece690
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-23 10:11:32 -0500

    Create 3_variables.py

---
+++ 3_variables.py
@@ -0,0 +1,17 @@
+## VARIABLES
+nombre = "Joan";
+
+## Case Sensitive
+Book = "Digital Fortress";
+book = "Dr. Jekyll & Mr. Hyde";
+
+index,_serie = 1, "Mr. Robot";
+
+print(book)
+print(index,_serie)
+print(nombre);
+
+## CONVENTIONS  
+book_name = "Fight Club"; #Snake Case
+bookName = "Secret Window"; #Camel Case
+BookName = "The Analyst"; #Pascal Case
