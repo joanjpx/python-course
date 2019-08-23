0f4e5cd8d5a47e672d353130d0ec6ee670611648
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-23 11:46:36 -0500

    Update 6_lists.py

--- 6_lists.py
+++ 6_lists.py
@@ -1,7 +1,16 @@
 demo_list = [1,"Hola",True,[4,5,6,1,9],12.3];
 
-colors = ["Red", "Green", "Blue", "Yellow"];
+colors = ["red", "green", "blue", "yellow"];
 
 
 number_list = list([1,2,3,4]);
-print(type(number_list));
+#print(type(number_list));
+
+range_list = list(range(1,100));
+#print(range_list);
+
+## GET ALL METHODS 
+#print(dir(colors));
+#print(len(colors));
+#print(colors[1]);
+print('red' in colors);
