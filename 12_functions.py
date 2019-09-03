6ce20a38ae89a0fb4a688754857802c289ed6fc7
Joan P. Alvarado <joansyn.s@gmail.com>
2019-09-03 14:50:15 -0500

    Update 12_functions.py

--- 12_functions.py
+++ 12_functions.py
@@ -1,6 +1,17 @@
 #FUNCTIONS
 
-def hello(parm):
+def hello(parm="User"):
     print(f"Hello World {parm}");
 
-hello("Joan");
+hello("");
+
+def add(numberOne=0,numberTwo=0):
+    return(numberOne+numberTwo);
+
+print(add(1,2));
+
+
+#Lambda Functions
+plus = lambda One,Two : One+Two
+
+print(plus(100,30))
