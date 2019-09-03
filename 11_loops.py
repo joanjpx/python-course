dc5f33cc5705387630c2e9d0a0928554e7021d02
Joan P. Alvarado <joansyn.s@gmail.com>
2019-09-03 13:09:03 -0500

    Update 11_loops.py

--- 11_loops.py
+++ 11_loops.py
@@ -2,8 +2,17 @@ array = list(range(1,100));
 
 #print(array);
 
+## FOR BASIC
+for i in range(1,10):
+    print (i);
+
+##Example 2
 for item in array:
     print(f"Number is:{item}");
     if item == 10:
         print("He llegado a 10");
         break;
+
+## Working with strings
+for letter in "Hello World":
+    print(letter);
